3a5568a18a58d583a834c96c7aec14287aff99a5
c4w <chen4w@sina.com>
2018-08-17 13:19:02 +0800

    sendTX todo

--- src/rest.js
+++ src/rest.js
@@ -4,12 +4,13 @@ class RestAPI {
     /**
      * 
      * @param {*} address 服务地址
-     * @param {*} protocols 协议
-     * @param {*} cb 回调函数
      */
     constructor(address) {
         this._address = address;
     }
+    /**
+     * 返回含区块高度的概要信息
+     */
     chaininfo(){
         var url = this._address+'chaininfo';
         return rp({
@@ -19,6 +20,10 @@ class RestAPI {
         })
     }
     //TODO repChain应该提供流式接口,走protobuf序列化
+    /**
+     * 根据区块高度获取区块数据
+     * @param {*} height 区块高度
+     */
     block(height){
         var url = this._address+'block/'+height;
         return rp({
@@ -26,6 +31,32 @@ class RestAPI {
             "uri": url,
             "json": true
         })
+    }
+/**
+ * 发送签名交易，流式提交
+ * @param {*} tx 待发送的交易
+ */
+    sendTX(tx){
+        var url = this._address+'/postTran';
+        return rp({
+            method: 'POST',
+            uri: url,
+            formData: {
+                // Like <input type="text" name="name">
+                name: 'Jenn',
+                // Like <input type="file" name="file">
+                file: {
+                    value: fs.createReadStream('test/test.jpg'),
+                    options: {
+                        filename: 'test.jpg',
+                        contentType: 'image/jpg'
+                    }
+                }
+            },
+            headers: {
+                /* 'content-type': 'multipart/form-data' */ // Is set automatically
+            }
+        });
     }
  }
 module.exports = RestAPI;
